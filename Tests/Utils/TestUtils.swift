c0911e1b03e8ec6cda83a8d886b3f24af685dd9a
Roman Roibu <romanroibu@gmail.com>
2019-06-16 23:37:31 +0200

    Add test assert for testing JSON equality

--- Tests/Utils/TestUtils.swift
+++ Tests/Utils/TestUtils.swift
@@ -1,3 +1,25 @@
 #if !canImport(XCTest)
 #error("This target should only be used in test targets.")
 #endif
+
+import Foundation
+import NonStandard
+import XCTest
+
+public func stringify(_ object: Any, options: JSONSerialization.WritingOptions = []) throws -> String {
+    let data = try JSONSerialization.data(withJSONObject: object, options: options)
+    return String(decoding: data, as: UTF8.self)
+}
+
+public func stringify<T: Encodable>(_ encodable: T, options: JSONSerialization.WritingOptions = []) throws -> String {
+    let data = try JSONEncoder().encode(encodable)
+    return String(decoding: data, as: UTF8.self)
+}
+
+//MARK:- Asserts
+
+public func AssertEqualJSON<T: Encodable>(_ expression1: @autoclosure () -> T, _ expression2: @autoclosure () -> [String: Any], _ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) {
+    XCTAssertEqual(try stringify(expression1()),
+                   try stringify(expression2()),
+                   message(), file: file, line: line)
+}
